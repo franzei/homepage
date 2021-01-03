5609a389eecc7484b981d5899377788d8e789e5b
Franz Maier <franz.r.maier@gmx.at>
2021-01-03 11:40:44 +0100

    änderung zwei

--- process.php
+++ process.php
@@ -11,5 +11,5 @@
     //send email
     mail($admin_email, "New Form Submission", $message . ' - ' . $phone, "From:" . $email);
 
-    header('Location: https://kotlechner.netlify.app/success.html');
+    header('Location: https://kotlechner.netlify.app');
   }
